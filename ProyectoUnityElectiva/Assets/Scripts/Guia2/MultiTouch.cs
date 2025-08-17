d6a7f76aa5349b021ee22df7a65c7594ecabe95e
UnknownFur <123666616+UnknownFur@users.noreply.github.com>
2025-08-17 13:14:36 -0500

    MultiTouch+Vpad Guia2

---
+++ ProyectoUnityElectiva/Assets/Scripts/Guia2/MultiTouch.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class MultiTouch : MonoBehaviour
+{
+    public TextMeshProUGUI InfoDisplay;
+    private int maxtapcount = 0;
+    private string multiTouchInfo;
+    private Touch theTouch;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InfoDisplay.SetText("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxtapcount = Input.touchCount;
+        multiTouchInfo = string.Format("Número de dedos: "+ maxtapcount + "\n");
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                theTouch = Input.GetTouch(0);
+                string info = "Touch " + i.ToString() + "\nCordenadas: \nX: " + theTouch.position.x + "\nY: " + theTouch.position.y + "\n";
+                multiTouchInfo += info;
+            }
+        }
+        InfoDisplay.SetText(multiTouchInfo);
+    }
+}
